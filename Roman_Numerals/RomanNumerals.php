7b605e7d4aa5051718c50848adaa732220051f6c
Antonio Jesús <ajesus@neventum.com>
2012-10-25 16:17:03 +0200

    First attemp

--- Roman_Numerals/RomanNumerals.php
+++ Roman_Numerals/RomanNumerals.php
@@ -2,5 +2,27 @@
 
 class RomanNumerals
 {
-	
+	protected $combination = array(
+		1  => 'I',
+		4  => 'IV',
+		5  => 'V',
+		9  => 'IX',
+		10 => 'X',
+		);
+
+	public function arabicToRoman($arabic) {
+
+		if(empty($arabic)) {
+		 	return;
+		}
+
+		return $this->units($arabic);
+
+		return 'Not valid number given';
+	}
+
+	public function units($arabic) {
+
+		return $this->combination[$arabic];
+	}
 }
--- Roman_Numerals/RomanNumeralsTest.php
+++ Roman_Numerals/RomanNumeralsTest.php
@@ -1,10 +1,36 @@
 <?php
 
+require_once __DIR__.'/RomanNumerals.php';
+
 class RomanNumeralsTest extends PHPUNIT_framework_TestCase
 {
 	protected $number;
 
 	public function setUp() {
 		$this->number = new RomanNumerals();
+	}
+
+	public function Provider() {
+		$cases = array(
+			array('', 0),
+			array('I', 1),
+			array('II', 2),
+			array('III', 3),
+			array('IV', 4),
+			array('V', 5),
+			array('VI', 6),
+			array('VII', 7),
+			array('VIII', 8),
+			array('IX', 9),
+			);
+
+		return $cases;
+	}
+
+	/**
+	 * @dataProvider Provider
+	 */
+	public function testArabicToRoman($expected, $arabic) {
+		$this->assertEquals($expected, $this->number->arabicToRoman($arabic));
 	}
 }
